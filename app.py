f2b8211c022b5efe267df794a5c895e0ca3ae1c6
imalkaperera13 <imalkaperera13@users.noreply.github.com>
2026-06-03 16:16:23 +0530

    changed the displaying words in app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from CI/CD Pipline v1")
+        self.wfile.write(b"Hello from CI/CD Pipline v1 , wuz up niggas!")
 
 HTTPServer(("",8080), Handler).serve_forever()  
