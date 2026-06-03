64272c43118a0843c7196f5e31602a165a0d0bc5
imalkaperera13 <imalkaperera13@users.noreply.github.com>
2026-06-03 16:47:02 +0530

    testing finalizing

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from CI/CD Pipline latest works!")
+        self.wfile.write(b"Hello from CI/CD Pipline latest works!Final Test")
 
 HTTPServer(("",8080), Handler).serve_forever()  
